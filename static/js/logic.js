88b212d943c7ef6d3630747ea8a3890cacc4715c
Dustin Elery <delery7@yahoo.com>
2021-04-17 10:15:38 -0400

    got the map to start rendering

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,9 +1,15 @@
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson"
 
-// / Perform a GET request to the query URL
+// "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+  
+// Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
+  console.log(data);
 });
 
 function createFeatures(earthquakeData) {
