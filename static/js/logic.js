bcab22eb38172346367bcb9b00d1de5457dfeb71
Dustin Elery <delery7@yahoo.com>
2021-04-19 19:35:33 -0400

    could not get the popup icon the change size and color

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,6 @@
-// Store our API endpoint inside queryUrl
+//DISCLOSURE: Most of this code was taken from a homework assignment.  This is largely from the Butler Data and Analytics course, modified to meet the homework requirments
+
+// Storing the API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_week.geojson"
   
 // Perform a GET request to the query URL
@@ -10,14 +12,23 @@ d3.json(queryUrl, function(data) {
 
 function createFeatures(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
+   // Give each feature a popup describing the place, time, Magnitude, and Depth of the earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + "Magnitude:" + (feature.properties.mag) + "</p>" +
-      "</h3><hr><p>" + "Depth:" + (feature.geometry.coordinates[2]) + "</p>" +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+      "</h3><hr><p>" + "Magnitude: " + (feature.properties.mag) + "</p>" +
+      "</h3><hr><p>" + "Depth: " + (feature.geometry.coordinates[2]) + "</p>" +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>"
+      // ,
+      //Could not figure out how to size or change color from bindPopup
+      // {
+      //   autoPanPaddingTopLeft: feature.properties.mag
+      // }
+      );
+      
   }
+
+
+
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
@@ -46,7 +57,7 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create our map, giving it the streetmap, center loading point, and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
       37.09, -95.71
@@ -54,5 +65,17 @@ function createMap(earthquakes) {
     zoom: 5,
     layers: [streetmap, earthquakes]
   });
+
+//   // Set the icon size by maginitude values
+//   var mapIcon = L.icon({
+//        iconSize:     [38, 95], // size of the icon
+//     // shadowSize:   [50, 64], // size of the shadow
+//     // iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     // shadowAnchor: [4, 62],  // the same for the shadow
+//     // popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
+
+// mapIcon.addTo(myMap);
+
 
 }
